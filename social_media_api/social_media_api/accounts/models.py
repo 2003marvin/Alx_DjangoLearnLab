e6c9444a029d835f1484f1e39fba354389615631
2003marvin <marvinmuthama03@gmail.com>
2025-12-12 13:24:24 +0300

    added accounts to settings.py

--- social_media_api/social_media_api/accounts/models.py
+++ social_media_api/social_media_api/accounts/models.py
@@ -1,15 +1,71 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.core.validators import RegexValidator
 
-class User(AbstractUser):
-    bio = models.TextField(blank=True, null=True)
-    profile_picture = models.ImageField(upload_to="profiles/", blank=True, null=True)
-    followers = models.ManyToManyField(
-        'self',
-        symmetrical=False,
-        related_name='following',
-        blank=True
-    )
+class CustomUser(AbstractUser):
+    bio = models.TextField(blank=True, null=False)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    followers = models.ManyToManyField("self", symmetrical=False, related_name="followers", blank=True)
 
     def __str__(self):
         return self.username
+    
+    profile_picture = models.ImageField(
+        _('profile picture'), 
+        upload_to='profile_pics/', 
+        blank=True, 
+        null=True
+    )
+    followers = models.ManyToManyField(
+        'self', 
+        symmetrical=False, 
+        related_name='following', 
+        blank=True
+    )
+    
+    # Optional: Phone number validation
+    phone_regex = RegexValidator(
+        regex=r'^\+?1?\d{9,15}$', 
+        message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."
+    )
+    phone_number = models.CharField(
+        validators=[phone_regex], 
+        max_length=17, 
+        blank=True, 
+        null=True
+    )
+    
+    # Additional profile fields
+    location = models.CharField(
+        _('location'), 
+        max_length=100, 
+        blank=True, 
+        null=True
+    )
+    
+    # Tracking user registration and last activity
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_activity = models.DateTimeField(auto_now=True)
+    
+    # Custom method to get full name
+    def get_full_name(self):
+        """
+        Returns the first_name plus the last_name, with a space in between.
+        """
+        full_name = f'{self.first_name} {self.last_name}'.strip()
+        return full_name or self.username
+
+    def __str__(self):
+        return self.username
+    
+    def get_followers_count(self):
+        return self.followers.count()
+    
+    def get_following_count(self):
+        return self.following.count()
+    
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+        ordering = ['-date_joined']
